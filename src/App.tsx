05840b3d27dd615c7ab9d2d607b1ce4bb6de553b
Egor O. Chindyasov <egor@chindyasov.ru>
2022-09-06 17:04:05 +0300

    changed a structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {
+    ChakraProvider, 
+    ColorMode, 
+    ImageProps, 
+    theme
+} from '@chakra-ui/react'
+import ColorModeSwitcher from './components/ColorModeSwitcher'
+import Dropdown from './ui/Dropdown'
+import {DropdownOptions} from './ui/Dropdown/type'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const IMAGE_PROPS: ImageProps = {
+    boxSize: '1.5rem',
+    borderRadius: 'full',
+    mr: '12px',
+    borderWidth: '2px',
+    borderStyle: 'solid',
 }
 
-export default App;
+export const App = () => {
+    const [colorMode, setColorMode] = useState<ColorMode>()
+
+    const dropdownOptions: DropdownOptions[] = [
+        {
+            title: 'All colors',
+            imageProps: {
+                borderColor: colorMode === 'light' ? 'black' : 'light',
+                ...IMAGE_PROPS
+            }
+        },
+        {
+            title: 'Green',
+            imageProps: {
+                background: 'green',
+                borderColor: 'green',
+                ...IMAGE_PROPS
+            }
+        },
+        {
+            title: 'Red',
+            imageProps: {
+                background: 'red',
+                borderColor: 'red',
+                ...IMAGE_PROPS
+            }
+        },
+    ]
+
+    return <ChakraProvider 
+        theme={theme}
+    >
+        <ColorModeSwitcher 
+            setColorMode={setColorMode}
+            justifySelf="flex-end" 
+        />
+        <Dropdown 
+            options={dropdownOptions}
+            settings={{w: '250px'}} 
+            title='Colors'
+            labelTitle='Color'
+        />
+    </ChakraProvider>
+}
